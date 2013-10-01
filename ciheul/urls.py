2eedfe605a50dc1753344818697a38fbb23a9c7a
Winnu Ayi Satria <winnuayi@gmail.com>
2013-10-01 09:29:53 +0700

    diff urls.py, try to ignore my branch and pull the latest one.

--- ciheul/urls.py
+++ ciheul/urls.py
@@ -19,11 +19,11 @@ v1_api.register(BusinessResource())
 urlpatterns = patterns(
     '',
     (r'^admin/', include(admin.site.urls)),
-    url(r'', include('social_auth.urls')),
-    url(r'', home.views.home),
     url(r'^dirban/members/$', dirban.views.members),
     url(r'^bigear/', include(v1_api.urls)),
     url(r'^bigear/$', bigear.views.home),
     url(r'^dirban/', include(v1_api.urls)),
     url(r'^dirban/$', dirban.views.home),
+    url(r'', home.views.home),
+    url(r'', include('social_auth.urls')),
 )
