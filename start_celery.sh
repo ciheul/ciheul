80af89fa7623c1f7e9da1b93df8e27c0ffaa93a2
Winnu Ayi Satria <winnuayi@gmail.com>
2014-05-13 10:14:32 +0700

    remove deleted files from track

--- start_celery.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ../..
-PROJECTS_DEV_DIR=$(pwd)
-echo $PROJECTS_DEV_DIR
-source $PROJECTS_DEV_DIR/virtualenv/ciheul/bin/activate
-
-cd $PROJECTS_DEV_DIR/www/ciheul
-exec celery -A ciheul.jendela24 worker -B --schedule celerybeat-schedule.db
